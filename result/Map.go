a8dd419ff78552787827400a70d8bacebf761200
João Nuno <joaonrb@gmail.com>
2023-12-26 12:33:35 +0100

    Minor change

--- result/Map.go
+++ result/Map.go
@@ -4,6 +4,7 @@ func Map[R1 any, R2 any, E error](
 	r1 Result[R1, E],
 	call func(result R1) Result[R2, E],
 ) (r2 Result[R2, E]) {
+	r1.result()
 	r1.Then(func(r R1) Result[R1, E] {
 		r2 = call(r)
 		return r1

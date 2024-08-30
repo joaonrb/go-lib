c807d1b9cade270ff7c5155697daee0dcaaba399
João Nuno <joao.baptista@devoteam.com>
2024-08-30 11:57:57 +0200

    Add tests for Result to Error

---
+++ convert/ResultToErrorOption_test.go
@@ -0,0 +1,21 @@
+package convert_test
+
+import (
+	"errors"
+	"github.com/joaonrb/go-lib/convert"
+	"github.com/joaonrb/go-lib/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestResultToErrorOptionShouldReturnNothingWhenResultIsOK(t *testing.T) {
+	result := convert.ResultToErrorOption(types.OK[int]{Value: 10})
+	require.IsType(t, types.Nothing[error]{}, result)
+}
+
+func TestResultToErrorOptionShouldReturnValueWhenResultIsError(t *testing.T) {
+	result := convert.ResultToErrorOption(types.Error[int]{Err: errors.New("an error")})
+	require.IsType(t, types.Value[error]{}, result)
+	assert.Equal(t, "an error", result.(types.Value[error]).This.Error())
+}

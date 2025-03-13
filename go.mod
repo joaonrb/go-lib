b836c16ffacd16377c0bc8a411c1e40953d626d7
João Nuno <joaonrb@gmail.com>
2025-03-13 13:42:01 +0100

    Add result, ok, and error

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joaonrb/go-lib
+
+go 1.21.0
---
+++ result/Error.go
@@ -0,0 +1,24 @@
+package result
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Error[T any, E error] struct {
+	Err E
+}
+
+func (err Error[T, E]) result() {}
+
+func (err Error[T, E]) Then(func(T) Result[T, E]) Result[T, E] {
+	return err
+}
+
+func (err Error[T, E]) Error(call func(E)) {
+	call(err.Err)
+}
+
+func (err Error[T, E]) String() string {
+	return fmt.Sprintf("Error[%s]{Err: %x}", reflect.TypeOf(err.Error).Name(), err.Err)
+}

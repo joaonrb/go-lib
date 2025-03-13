634c3ff9ef5835b424ba2275ab0717f35e3ef10e
João Nuno <joaonrb@gmail.com>
2025-03-13 13:42:06 +0100

    Fix ResultToOption

--- convert/ResultToOption.go
+++ convert/ResultToOption.go
@@ -1,15 +1,18 @@
 package convert
 
-import "github.com/joaonrb/go-lib/types"
+import (
+	"github.com/joaonrb/go-lib/log"
+	"github.com/joaonrb/go-lib/types"
+)
 
 func ResultToOption[T any](result types.Result[T]) (option types.Option[T]) {
 	result.
 		WhenOK(func(t T) {
-			option = types.Nothing[T]{}
-			option = types.Value[T]{This: err}
+			option = types.Value[T]{This: t}
 		}).
 		WhenError(func(err error) {
-			log
+			var t T
+			log.Debug("ResultToOption converted error %s to Nothing[%T]", err, t)
 			option = types.Nothing[T]{}
 		})
 	return
